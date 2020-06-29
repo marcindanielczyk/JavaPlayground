b52c61c1d77cfd5d0b9bd56588e24a7e9d6ad144
Marcin Danielczyk <marcindanielczyk@gmail.com>
2020-06-30 01:16:52 +0200

    Remove files

--- ConstructorOverloading/src/Main.java
+++ ConstructorOverloading/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //moze zamiast public nie byc niczego, czemu
+        //moze zamiast public nie byc niczego
         class Tv {
             private String modelName;
             private long serialNumber;
